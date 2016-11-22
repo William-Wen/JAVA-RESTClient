226a84bfc4697eba89c3b6dc36335a672cf610ea
William <william.wen@tac.com.tw>
2016-11-23 06:12:17 +0800

    Lecture 20. Test the Service using a Java Client. Section 04. Write a REST Service. The "DELETE" method is not working. All section is finished. here.

--- src/domain/RESTClient.java
+++ src/domain/RESTClient.java
@@ -4,7 +4,9 @@ import java.net.URI;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 
@@ -43,5 +45,57 @@ public class RESTClient {
 		return oWebTarget.request(mediaType)
 				.get();
 	}
+	
+	public Response get(String food, String mediaType) {
+		return oWebTarget.path(food)
+				.request(mediaType)
+				.get();
+	}
+	
+	public Response post(String name, int calories, String servingSize) {
+		Food oFood = new Food(name, calories, servingSize);
+		Entity<Food> oEntityFood = Entity.entity(oFood, MediaType.APPLICATION_XML);
+		return oWebTarget.request()
+				.post(oEntityFood);
+	}
+	
+	public Response put(String name, int calories, String servingSize) {
+		Food oFood = new Food(name, calories, servingSize);
+		Entity<Food> oEntityFood = Entity.entity(oFood, MediaType.APPLICATION_XML);
+		return oWebTarget.request()
+				.put(oEntityFood);
+	}
+	
+	public Response delete(String name) {
+		return oWebTarget.path(name)
+				.request()
+				.delete();
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test/domain/RESTClientTest.java
+++ test/domain/RESTClientTest.java
@@ -11,6 +11,7 @@ import utility.Utility;
 
 public class RESTClientTest {
 
+
 	@Test
 	public void getAllTest() {
 		// call the web service.
@@ -21,5 +22,77 @@ public class RESTClientTest {
 		// test the response.
 		assertTrue(Utility.getStatusCode(oResponse) == Utility.OK_STATUS);
 	}
+	
+	private static final String FOOD_NAME = "Beans";
+	
+	@Test
+	public void getTest(){
+		// call the web service
+		RESTClient oRestClient = new RESTClient();
+		Response oResponse = oRestClient.get(FOOD_NAME, MediaType.APPLICATION_JSON);
+		Utility.printResponse(oResponse);
+		
+		// test the response
+		int oResponseCode = Utility.getStatusCode(oResponse);
+		boolean OK = (oResponseCode == Utility.OK_STATUS);
+		boolean NOT_FOUND = (oResponseCode == Utility.NOT_FOUND_STATUS);
+		assertTrue(NOT_FOUND || OK); 
+		
+	}
+	
+	@Test
+	public void postTest() {
+		// create a "turnip"
+		RESTClient oClient = new RESTClient();
+		Response oResponse = oClient.post("turnip", 50, "3 slices");
+		Utility.printResponse(oResponse);
+		
+		// test the response
+		int oResponseCode = Utility.getStatusCode(oResponse);
+		boolean NOT_MODIFIED = (oResponseCode == Utility.NOT_MODIFIED_STATUS);
+		boolean CREATED = (oResponseCode == Utility.CREATED_STATUS);
+		assertTrue(NOT_MODIFIED || CREATED);
+	}
+	
+	@Test
+	public void putTest() {
+		// change the "turnip" properties
+		RESTClient oRestClient = new RESTClient();
+		Response oResponse = oRestClient.put("turnip", 80, "4 slices");
+		Utility.printResponse(oResponse);
+		
+		// test the response
+		assertTrue(Utility.getStatusCode(oResponse) == Utility.OK_STATUS);				
+	}
+	
+	@Test
+	public void deleteTest() {
+		// delete a "turnip"
+		RESTClient oRestClient = new RESTClient();
+		Response oResponse = oRestClient.delete("turnip");
+		Utility.printResponse(oResponse);
+		
+		// test the response
+		int oResponseCode = Utility.getStatusCode(oResponse);
+		boolean NO_CONTENT = (oResponseCode == Utility.NO_CONTENT_STATUS);
+		boolean NOT_FOUND = (oResponseCode == Utility.NOT_FOUND_STATUS);
+		assertTrue(NOT_FOUND || NO_CONTENT); 
+		
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
